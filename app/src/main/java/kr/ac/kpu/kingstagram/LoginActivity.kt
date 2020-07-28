216a222b9042bd1f5510c9f1ff05ec6750163417
DESKTOP-DTPHEP0\user <qweqwe136@naver.com>
2020-07-28 20:28:12 +0900

    회원가입 링크

--- app/src/main/java/kr/ac/kpu/kingstagram/LoginActivity.kt
+++ app/src/main/java/kr/ac/kpu/kingstagram/LoginActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kpu.kingstagram
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.util.Linkify
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -34,15 +35,13 @@ class LoginActivity : AppCompatActivity() {
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
                         Log.d("login" , "signInWithEmail:success")
-                        Toast.makeText(baseContext, "Authentication success.",
-                            Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(baseContext, "Authentication success.", Toast.LENGTH_SHORT).show()
                         val user = auth.currentUser
                         updateUI(user)
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w("login", "signInWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
                         updateFail(null)
                         // ...
                     }
@@ -51,6 +50,12 @@ class LoginActivity : AppCompatActivity() {
                 }
 
         }
+        var intent = Intent(this,SignUpActivity::class.java)
+        content_signUp.setOnClickListener {
+            startActivity(intent)
+        }
+
+
     }
 
     public override fun onStart() {
