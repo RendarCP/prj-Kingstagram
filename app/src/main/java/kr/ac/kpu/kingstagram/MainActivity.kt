581c49691f2716d72e33eb15628b1241d09cb176
rendarCB <tjddnr0217@gmail.com>
2020-07-28 11:19:57 +0900

    bottom navigation 추가 및 title bar 제거

--- app/src/main/java/kr/ac/kpu/kingstagram/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/kingstagram/MainActivity.kt
@@ -2,10 +2,38 @@ package kr.ac.kpu.kingstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.ac.kpu.kingstagram.navigation.DetailViewFragment
+import kr.ac.kpu.kingstagram.navigation.SearchFragment
+import kr.ac.kpu.kingstagram.navigation.UserFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home -> {
+                val detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                val searchFragment = SearchFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,searchFragment).commit()
+                return true
+            }
+            R.id.action_profile -> {
+                val userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
     }
 }
